24f8f6a133761bdf3dbf03e34eaf49c24c28462f
livia <livian8929@gmail.com>
2025-10-01 07:51:44 -0300

    primeiro commit

---
+++ demojava/src/test/java/com/web/demojava/DemojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.demojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
